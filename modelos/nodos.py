fd188551d28a47e4eb1e296f2e5dca1f0b0491b2
Daniel <daniel.odontolegal@gmail.com>
2026-06-13 18:09:55 -0300

    Cria o arquivo nodos.py
    Insere o código das classes nodofila e nodolista.

---
+++ modelos/nodos.py
@@ -0,0 +1,9 @@
+class NodoFila:
+    def __init__(self, figurinha):
+        self.figurinha = figurinha  # guarda o objeto figurinha
+        self.proximo = None         # No início não aponta pra ninguém
+
+class NodoLista:
+    def __init__(self, figurinha):
+        self.figurinha = figurinha  # guarda objeto figurinha
+        self.proximo = None         # no início não aponta pra ninguém
